bee166c3751da4693edf9c881c470bff0f7b6d51
JMeter User <jmeter-user@jmeter.(none)>
2014-12-04 09:06:53 -0300

    rspec working

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,9 +2,13 @@ class Post < ActiveRecord::Base
 
 
   def sleep_for_given_amount_of_time
+    
+    sleep 3
     true
   end
  
   def sleep_some_more
+    sleep 10
+    true
   end
 end
--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -5,13 +5,22 @@ describe Post do
     @post = Post.new
   end
 
-  describe "when method is called, should sleep for a given amount of time" do
+  describe "when method is called, should sleep for a given amount of time then return tru" do
     it "sleeps for a given amount of time" do
       post = Post.new
       result = post.sleep_for_given_amount_of_time
       expect(result).to eq true
     end
   end
+
+  describe "when method is called, should sleep some more then return true" do
+    it "sleeps for a given amount of time" do
+      post = Post.new
+      result = post.sleep_some_more
+      expect(result).to eq true
+    end
+  end
+
 
   describe "#new" do
     it "creates new instance of Post" do
