51600447bfd1c1f5f10545e73914a642e2465b79
JMeter User <jmeter-user@jmeter.(none)>
2014-12-04 08:51:21 -0300

    rspec funfando

--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -5,18 +5,18 @@ describe Post do
     @post = Post.new
   end
 
-
-#  describe "#sleep_for_given_amount_of_time" do
-#    it "sleeps for a given amount of time"
-#      post = Post.new
-#      result = post.sleep_for_given_amount_of_time
-#      result.should eql true
-#    end
-#  end
-
-end
-  describe "#new" do
-    it "creates new instance of Post" do
-      @post.should be_an_instance_of Post
+  describe "when method is called, should sleep for a given amount of time" do
+    it "sleeps for a given amount of time" do
+      post = Post.new
+      result = post.sleep_for_given_amount_of_time
+      expect(result).to eq true
     end
   end
+
+  describe "#new" do
+    it "creates new instance of Post" do
+      expect(@post).to  be_an_instance_of Post
+    end
+  end
+end
+
